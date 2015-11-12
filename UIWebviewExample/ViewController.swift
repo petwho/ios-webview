18fc027a5f48b2dd7b5f6317d3299a158580125d
khanhth <khanhth.evolableasia@gmail.com>
2015-11-12 15:46:01 +0700

    Implement UIWebView

--- UIWebviewExample/ViewController.swift
+++ UIWebviewExample/ViewController.swift
@@ -10,16 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
+    @IBOutlet weak var webView: UIWebView!;
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        // Method 1
+//        let requestURL = NSURL(string: "http://www.sourcefreeze.com");
+//        let requestObj = NSURLRequest(URL: requestURL!);
+//        webView.loadRequest(requestObj);
+
+        // Method 2
+//        let localfilePath = NSBundle.mainBundle().URLForResource("home", withExtension: "html");
+//        let myRequest = NSURLRequest(URL: localfilePath!);
+//        webView.loadRequest(myRequest);
+        // Method 3
+        let webFrame = CGRectMake(0, 0, UIScreen.mainScreen().bounds.width, UIScreen.mainScreen().bounds.height)
+        let webview:UIWebView = UIWebView(frame: webFrame)
+        webview.loadRequest(NSURLRequest(URL: NSURL(string: "http://www.sourcefreeze.com")!))
+        self.view.addSubview(webview)
+
+        let htmlTitle = webView.stringByEvaluatingJavaScriptFromString("document.title");
+        print(htmlTitle)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
